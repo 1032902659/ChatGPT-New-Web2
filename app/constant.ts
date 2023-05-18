59171acd4f9c713902d6a0ef8e1dc85ff7b40eb2
lishixuan <94160612+1032902659@users.noreply.github.com>
2023-05-18 19:24:55 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "1032902659";
-export const REPO = "ChatGPT-New-Web";
+export const REPO = "ChatGPT-New-Web2";
 export const REPO_URL = `https://qm.qq.com/cgi-bin/qm/qr?k=BGdGHQfXBbPTifZPjZA8xC8aOBu2Q1Fs&noverify=0`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `https://github.com/1032902659/ChatGPT-NEW-Web#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
