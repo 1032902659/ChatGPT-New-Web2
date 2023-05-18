57439e12a131e9f083d70f5f727009250bbafac4
lishixuan <94160612+1032902659@users.noreply.github.com>
2023-05-18 19:29:58 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "1032902659";
 export const REPO = "ChatGPT-New-Web2";
 export const REPO_URL = `https://qm.qq.com/cgi-bin/qm/qr?k=BGdGHQfXBbPTifZPjZA8xC8aOBu2Q1Fs&noverify=0`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `https://github.com/1032902659/ChatGPT-NEW-Web#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const UPDATE_URL = `https://github.com/1032902659/ChatGPT-NEW-Web2#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
